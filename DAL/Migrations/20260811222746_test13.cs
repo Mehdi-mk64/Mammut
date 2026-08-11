e15b7bc46a3a9e1431227b94ff83d586557dd962
moradii.mehdii64@gmail.com <moradii.mehdii64@gmail.com>
2026-08-12 00:28:33 +0200

    USERID

---
+++ DAL/Migrations/20260811222746_test13.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class test13 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Security",
+                table: "ApplicationUser",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "AQAAAAEAACcQAAAAELDettaU5Kme+ZbVgKULFy6VVECc5TmQiKN+ATbVW1fivNXUQV3j+dsAmfdwJYFYWA==");
+
+            migrationBuilder.UpdateData(
+                schema: "Security",
+                table: "ApplicationUser",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PasswordHash",
+                value: "AQAAAAEAACcQAAAAENEWUXADh8n9F9Mct8P19Owit+eqqTkHHWe5oUjpBod9eczN7FuXTqVPikhcE7TXWQ==");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Security",
+                table: "ApplicationUser",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "AQAAAAEAACcQAAAAEP5XzLq2DYrP0WLR9PrrBwV8ZsUmPZCcTVnD81i/CpQLAvwpl2C1cHfgK0TSAdpU/A==");
+
+            migrationBuilder.UpdateData(
+                schema: "Security",
+                table: "ApplicationUser",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PasswordHash",
+                value: "AQAAAAEAACcQAAAAEIc4QrHi95CMExlYvx48iwpidcJMwW9FnvE7eP8A84I32+IpZRNkytVnnLpvzfnJag==");
+        }
+    }
+}
--- DAL/Repository/Basic/SMS/MessageLogRepository.cs
+++ DAL/Repository/Basic/SMS/MessageLogRepository.cs
@@ -46,76 +46,76 @@ namespace DAL.Repository.Basic.SMS
 
 
 
-        public async Task<PagedResultDto<MessageLogReportDto>> SearchReportAsync( string? phoneNumber, string? personCode,
-            int page, int pageSize, CancellationToken cancellationToken = default)
-        {
-            if (page < 1)
-                page = 1;
+        //public async Task<PagedResultDto<MessageLogReportDto>> SearchReportAsync( string? phoneNumber, string? personCode,
+        //    int page, int pageSize, CancellationToken cancellationToken = default)
+        //{
+        //    if (page < 1)
+        //        page = 1;
 
-            if (pageSize < 1)
-                pageSize = 15;
+        //    if (pageSize < 1)
+        //        pageSize = 15;
 
-            if (pageSize > 50)
-                pageSize = 50;
+        //    if (pageSize > 50)
+        //        pageSize = 50;
 
-            var query = TableNoTracking.AsQueryable();
+        //    var query = TableNoTracking.AsQueryable();
 
-            if (!string.IsNullOrWhiteSpace(phoneNumber))
-            {
-                phoneNumber = phoneNumber.Trim();
+        //    if (!string.IsNullOrWhiteSpace(phoneNumber))
+        //    {
+        //        phoneNumber = phoneNumber.Trim();
 
-                query = query.Where(x =>
-                    x.MessageLog_MessageSend
-                        .PhoneNummber
-                        .Nummber
-                        .Contains(phoneNumber));
-            }
+        //        query = query.Where(x =>
+        //            x.MessageLog_MessageSend
+        //                .PhoneNummber
+        //                .Nummber
+        //                .Contains(phoneNumber));
+        //    }
 
-            if (!string.IsNullOrWhiteSpace(personCode))
-            {
-                personCode = personCode.Trim();
+        //    if (!string.IsNullOrWhiteSpace(personCode))
+        //    {
+        //        personCode = personCode.Trim();
 
-                query = query.Where(x =>
-                    x.MessageLog_MessageSend
-                        .PhoneNummber
-                        .Phone_Person
-                        .PersonCode
-                        .Contains(personCode));
-            }
+        //        query = query.Where(x =>
+        //            x.MessageLog_MessageSend
+        //                .PhoneNummber
+        //                .Phone_Person
+        //                .PersonCode
+        //                .Contains(personCode));
+        //    }
 
-            var totalCount = await query.CountAsync(cancellationToken);
+        //    var totalCount = await query.CountAsync(cancellationToken);
 
-            var items = await query
-                // جدیدترین به قدیمی‌ترین
-                .OrderByDescending(x => x.ActionDateTime)
-                .Skip((page - 1) * pageSize)
-                .Take(pageSize)
-                .Select(x => new MessageLogReportDto
-                {
-                    PersonCode = x.MessageLog_MessageSend.PhoneNummber.Phone_Person.PersonCode,
+        //    var items = await query
+        //        // جدیدترین به قدیمی‌ترین
+        //        .OrderByDescending(x => x.ActionDateTime)
+        //        .Skip((page - 1) * pageSize)
+        //        .Take(pageSize)
+        //        .Select(x => new MessageLogReportDto
+        //        {
+        //            PersonCode = x.MessageLog_MessageSend.PhoneNummber.Phone_Person.PersonCode,
 
-                    PhoneNumber =x.MessageLog_MessageSend.PhoneNummber.Nummber,
+        //            PhoneNumber =x.MessageLog_MessageSend.PhoneNummber.Nummber,
 
-                    SendStatus = x.MessageLog_SendStatus.Title,
+        //            SendStatus = x.MessageLog_SendStatus.Title,
 
-                    StatusCodeReturn = x.StatusCodeReturn,
+        //            StatusCodeReturn = x.StatusCodeReturn,
 
-                    ActionDateTime = x.ActionDateTime
-                })
+        //            ActionDateTime = x.ActionDateTime
+        //        })
 
-                .ToListAsync(cancellationToken);
+        //        .ToListAsync(cancellationToken);
 
-            return new PagedResultDto<MessageLogReportDto>
-            {
-                Items = items,
+        //    return new PagedResultDto<MessageLogReportDto>
+        //    {
+        //        Items = items,
 
-                Page = page,
+        //        Page = page,
 
-                PageSize = pageSize,
+        //        PageSize = pageSize,
 
-                TotalCount = totalCount
-            };
-        }
+        //        TotalCount = totalCount
+        //    };
+        //}
 
         public async Task<PagedResultDto<MessageLogReportDto>> SearchReportAsync( string? phoneNumber, string? personCode,
              int page, int pageSize, int currentUserID, bool isAdmin, CancellationToken cancellationToken = default)
